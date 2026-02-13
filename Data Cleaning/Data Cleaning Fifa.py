085c2f7b30406eaed04f72fe5a707a36b926e5b2
Sketch488 <sewruttanshared6@gmail.com>
2026-02-13 19:27:17 -0300

    Slight Changes

--- Data Cleaning/Data Cleaning Fifa.py
+++ Data Cleaning/Data Cleaning Fifa.py
@@ -10,16 +10,11 @@ if os.path.exists(filepath):
     #Proper the longname column
     data["LongName"] = data["LongName"].astype(str).str.strip().str.title()
 
-    #data["LongName"] = data["LongName"].apply(lambda x: x.encode("latin-1").decode("utf-8-sig") if isinstance(x, str) else x)
-    #for col in data.select_dtypes(include=["object"]).columns:
-        #data[col] = data[col].apply(
-            #lambda x: x.encode("latin-1").decode("utf-8-sig") if isinstance(x, str) else x)
-    
-    #Sort, remove duplicates, and handle missing values
-    data = data.sort_values("LongName").reset_index(drop=True)
-    data = data.drop_duplicates(subset=["LongName", "Positions"], keep="first").reset_index(drop=True)
-    data = data.dropna(subset=["LongName", "Positions"]).reset_index(drop=True)
-    #data = data.dropna.reset_index(drop=True)
+    data = (
+        data.sort_values("LongName").reset_index(drop=True)
+        .drop_duplicates(subset=["LongName", "Positions"], keep="first").reset_index(drop=True)
+        .dropna(subset=["LongName", "Positions"]).reset_index(drop=True)
+    )
 
     data.to_csv("FifaV1_Sorted.csv", encoding="utf-8-sig", index=False)
 
